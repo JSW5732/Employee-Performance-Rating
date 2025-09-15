7098a9062d6b2daba6ec3e7ae2c32e09fabeb62e
JSW5732 <jsw5732@psu.edu>
2025-09-15 14:45:33 -0400

    mssql

--- employee-performance-rating-node/src/db/index.js
+++ employee-performance-rating-node/src/db/index.js
@@ -1,11 +1,16 @@
-const mysql = require('mysql2/promise');
+const sql = require('mssql');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    options: {
+        encrypt: false // Use true if connecting from Azure
+    }
+};
+
+const pool = new sql.ConnectionPool(config);
 
 module.exports = pool;
